9bc37eb511e8e6229e1f41f0e5a86f0b58e5a562
JoshMutsaers <muts3389@gmail.com>
2026-02-06 14:48:12 -0500

    Finished the methods in Bourd.java

--- Server/Board.java
+++ Server/Board.java
@@ -56,4 +56,70 @@ public class Board {
         }
         return pinsOutput;
     }
+
+    // Post a new note to the board
+    public synchronized String post(int x, int y, String colour, String message) {
+        if (x < 0 || y < 0 || x + this.noteWidth > this.boardWidth || y + this.noteHeight > this.boardHeight) {
+            return "ERROR OUT_OF_BOUNDS Note extends beyond the size of the board";
+        }
+        if (!this.colours.contains(colour)) {
+            return "ERROR COLOUR_NOT_SUPPORTED Colour is not in the valid list";
+        }
+        for (Note note : notes) {
+            if (note.getX() == x && note.getY() == y) {
+                return "ERROR COMPLETE_OVERLAP A note already exists at these coordinates";
+            }
+        }
+        notes.add(new Note(x, y, noteWidth, noteHeight, colour, message));
+        return "OK";
+    }
+
+    // Add a pin to the board
+    public synchronized String pin(int x, int y) {
+        for (Note note : notes) {
+            if (note.contains(x, y)) {
+                pins.add(new Pin(x, y));
+                return "OK";
+            }
+        }
+        return "ERROR NO_NOTE_AT_COORDINATE No note contains the given point";
+    }
+
+    // Remove a pin from the board
+    public synchronized String unpin(int x, int y) {
+        for (Pin pin : pins) {
+            if (x == pin.getX() && y == pin.getY()) {
+                pins.remove(pin);
+                return "OK";
+            }
+        }
+        return "ERROR PIN_NOT_FOUND No pin exists at the given coordinates";
+    }
+    
+    // Returns a list of notes that meet the given conditions
+    public synchronized String getNotes(String colour, Integer containsX, Integer containsY, String refersTo) {
+        String output = "OK ";
+        String outputNotes = "";
+        String pinnedStatus;
+        int count = 0;
+        boolean passes;
+        for (Note note : notes) {
+            passes = true;
+            if (colour != null && !note.getColour().equals(colour)) {
+                passes = false;
+            }
+            if (containsX != null && containsY != null && !note.contains(containsX, containsY)) {
+                passes = false;
+            }
+            if (refersTo != null && !note.getMessage().contains(refersTo)) {
+                passes = false;
+            }
+            if (passes) {
+                count++;
+                pinnedStatus = isNotePinned(note.getX(), note.getY()) ? "PINNED" : "UNPINNED";
+                outputNotes += "\nNOTE " + note.getX() + " " + note.getY() + " " + note.getColour() +  " " + pinnedStatus + " " + note.getMessage();
+            }
+        }
+        return output + count + outputNotes;
+    }
 }
